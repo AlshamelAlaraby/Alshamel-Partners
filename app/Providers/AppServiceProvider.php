a7bf2a0ec3a0831e17a7c4733de0b582ac619433
Mostafa1712002 <ebrahimmostafa695@gmail.com>
2022-11-28 19:07:40 +0200

    update to laravel 9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,22 +2,7 @@
 
 namespace App\Providers;
 
-use App\Repositories\Branch\BranchRepository;
-use App\Repositories\Branch\BranchRepositoryInterface;
-use App\Repositories\User\UserRepository;
-use App\Repositories\User\UserRepositoryInterface;
-use App\Repositories\Partner\PartnerRepository;
-use App\Repositories\Partner\PartnerRepositoryInterface;
-use App\Repositories\Serial\SerialRepository;
-use App\Repositories\Serial\SerialRepositoryInterface;
-use App\Repositories\Company\CompanyRepository;
-use App\Repositories\Company\CompanyRepositoryInterface;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-
-use App\Repositories\Module\ModuleInterface;
-use App\Repositories\Module\ModuleRepository;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,17 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-
-        $this->app->bind(PartnerRepositoryInterface::class, PartnerRepository::class);
-
-        $this->app->bind(SerialRepositoryInterface::class, SerialRepository::class);
-        $this->app->bind(CompanyRepositoryInterface::class, CompanyRepository::class);
-
-        $this->app->bind(ModuleInterface::class, ModuleRepository::class);
-
-        $this->app->bind(BranchRepositoryInterface::class, BranchRepository::class);
-
+        //
     }
 
     /**
@@ -48,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //
     }
 }
