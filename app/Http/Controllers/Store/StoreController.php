b765647d7ceffee00e182fd4fd69f50657b9b7ef
Mostafa1712002 <ebrahimmostafa695@gmail.com>
2022-11-29 18:57:28 +0200

    finish stores

---
+++ app/Http/Controllers/Store/StoreController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use App\Http\Requests\Store\StoreStoreRequest;
+use App\Http\Requests\Store\UpdateStoreRequest;
+use App\Http\Resources\Store\StoreResource;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class StoreController extends Controller
+{
+    public function __construct(private \App\Repositories\Store\StoreInterface$modelInterface)
+    {
+        $this->modelInterface = $modelInterface;
+    }
+
+    public function find($id)
+    {
+        $model = cacheGet('stores_' . $id);
+        if (!$model) {
+            $model = $this->modelInterface->find($id);
+            if (!$model) {
+                return responseJson(404, __('message.data not found'));
+            } else {
+                cachePut('stores_' . $id, $model);
+            }
+        }
+        return responseJson(200, 'success', new StoreResource($model));
+    }
+
+    public function all(Request $request)
+    {
+        if (count($_GET) == 0) {
+            $models = cacheGet('stores');
+            if (!$models) {
+                $models = $this->modelInterface->all($request);
+                cachePut('stores', $models);
+            }
+        } else {
+            $models = $this->modelInterface->all($request);
+        }
+
+
+        return responseJson(200, 'success', StoreResource::collection($models['data']), $models['paginate'] ? getPaginates($models['data']) : null);
+    }
+
+    public function create(StoreStoreRequest $request)
+    {
+        $model = $this->modelInterface->create($request);
+        return responseJson(200, 'success');
+    }
+
+    public function update(UpdateStoreRequest $request, $id)
+    {
+        $model = $this->modelInterface->find($id);
+        if (!$model) {
+            return responseJson(404, __('message.data not found'));
+        }
+        $model = $this->modelInterface->update($request, $id);
+
+        return responseJson(200, 'success');
+    }
+
+    public function delete($id)
+    {
+        $model = $this->modelInterface->find($id);
+        if (!$model) {
+            return responseJson(404, __('message.data not found'));
+        }
+        if ($model->have_children) {
+            return responseJson(400, __('message.parent have children'));
+        }
+        $this->modelInterface->delete($id);
+
+        return responseJson(200, 'success');
+    }
+
+}
---
+++ app/Http/Requests/Store/UpdateStoreRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\Store;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'string|max:255',
+            'name_e' => 'string|max:255',
+            'company_id' => 'integer|exists:companies,id',
+            'branch_id' => 'integer|exists:branches,id',
+            'is_active' => 'nullable|in:active,inactive',
+
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+
+    public function messages()
+    {
+        return [
+            'name.string' => __('message.field must be string'),
+            'name.max' => __('message.field must be less than 255 character'),
+
+            'name_e.string' => __('message.field must be string'),
+            'name_e.max' => __('message.field must be less than 255 character'),
+            'company_id.integer' => __('message.field must be integer'),
+            'company_id.exists' => __('message.field must be exists'),
+
+            'branch_id.integer' => __('message.field must be integer'),
+            'branch_id.exists' => __('message.field must be exists'),
+        ];
+    }
+}
