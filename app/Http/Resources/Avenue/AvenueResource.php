e92a854a68eb561fd484ec60f37fd78df539995d
ahmed elesawy <ahmed.elesawy202020@gmail.com>
2022-12-11 10:58:31 +0200

    last update

--- app/Http/Resources/Avenue/AvenueResource.php
+++ app/Http/Resources/Avenue/AvenueResource.php
@@ -23,6 +23,7 @@ class AvenueResource extends JsonResource
             'name_e' => $this->name_e,
             'is_active' => $this->is_active,
             "city" => new CityResource($this->city),
+
             "governorate" => new GovernorateResource($this->governorate),
             'country' => new CountryResource($this->country),
             'created_at' => $this->created_at,
