afc722b50b2a5f2869ebd0032973dfca1de8e028
miladtut <miladtut2@gmail.com>
2022-12-14 10:31:07 +0200

    symbol to res

--- Modules/RealEstate/Routes/api.php
+++ Modules/RealEstate/Routes/api.php
@@ -18,27 +18,27 @@ Route::middleware('auth:api')->get('/realestate', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix ('real-estate')->group (function (){
+Route::prefix ('real-estate')->group (function () {
 //    Route::middleware ('auth:sanctum')->group (function (){
 //        Route::resource ('property-types','PropertyTypeController')->except ('edit','create');
 //    });
 
-    Route::resource ('property-types','PropertyTypeController')->except ('edit','create');
-    Route::get ('property-types/logs/{id}','PropertyTypeController@logs');
+    Route ::resource ( 'property-types' , 'PropertyTypeController' ) -> except ( 'edit' , 'create' );
+    Route ::get ( 'property-types/logs/{id}' , 'PropertyTypeController@logs' );
 
-    Route::resource ('rlst-buildings','RlstBuildingController')->except ('edit','create');
-    Route::get ('rlst-buildings/logs/{id}','RlstBuildingController@logs');
+    Route ::resource ( 'rlst-buildings' , 'RlstBuildingController' ) -> except ( 'edit' , 'create' );
+    Route ::get ( 'rlst-buildings/logs/{id}' , 'RlstBuildingController@logs' );
 
-    Route::resource ('rlst-units','RlstUnitController')->except ('edit','create');
-    Route::get ('rlst-units/logs/{id}','RlstUnitController@logs');
+    Route ::resource ( 'rlst-units' , 'RlstUnitController' ) -> except ( 'edit' , 'create' );
+    Route ::get ( 'rlst-units/logs/{id}' , 'RlstUnitController@logs' );
 
-    Route::resource ('rlst-unit-status','RlstUnitStatusController')->except ('edit','create');
-    Route::get ('rlst-unit-status/logs/{id}','RlstUnitStatusController@logs');
+    Route ::resource ( 'rlst-unit-status' , 'RlstUnitStatusController' ) -> except ( 'edit' , 'create' );
+    Route ::get ( 'rlst-unit-status/logs/{id}' , 'RlstUnitStatusController@logs' );
 
-    Route::resource ('rlst-wallet-building','RlstWalletBuildingController')->except ('edit','create');
-    Route::get ('rlst-wallet-building/logs/{id}','RlstWalletBuildingController@logs');
+    Route ::resource ( 'rlst-wallet-building' , 'RlstWalletBuildingController' ) -> except ( 'edit' , 'create' );
+    Route ::get ( 'rlst-wallet-building/logs/{id}' , 'RlstWalletBuildingController@logs' );
 
-
+});
 
 Route::group(['prefix' => 'rlst', 'namespace' => 'Modules\RealEstate\Http\Controllers'], function () {
     Route::group(['prefix' => 'owners'], function () {
