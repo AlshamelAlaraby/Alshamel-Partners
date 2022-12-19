ad3514aac01178e7453def8919fa193dadf0edf0
Mostafa1712002 <mostafaibrahim1712002@gmail.com>
2022-12-19 13:11:59 +0200

    is d

---
+++ Modules/RealEstate/Http/Requests/CreateRlstUnitContractRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\RealEstate\Http\Requests;
+
+use App\Traits\ValidationTrait;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRlstUnitContractRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            "unit_code" => "required|exists:tree_properties,code",
+        ];
+    }
+
+
+    public function messages()
+    {
+        return [
+            "unit_code.required" => __("message.field is required"),
+            "unit_code.exists" => __("message.field must be exists"),
+
+        ];
+    }
+}
